59aca1f69f2194520a8d5b5932695355f0efd37e
Anderson-Ramos-Iman <anderson.ramos@tecsup.edu.pe>
2026-05-09 21:03:49 -0500

    new logs

--- src/app.js
+++ src/app.js
@@ -2,11 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import userRoutes from './routes/users.routes.js';
 
+// Initialize Express app
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use('/api/users', userRoutes);
 
+// Health check route
 export default app;
